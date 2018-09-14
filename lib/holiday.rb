71f0d75f00d5ec2f3ab87a800d597698e3c82de0
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 04:12:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,15 +18,14 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_supplies[:summer][:fourth_of_july][1]
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-holiday_supplies.each do |season, holiday|
-  holiday << supply
+holiday_hash[:winter][:new_years].push(supply)
   end
 end
 

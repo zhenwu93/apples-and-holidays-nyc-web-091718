dc3722d740d0be0e58c96e7a5e9529622f2a431e
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 04:28:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.collect do |season, value|
+    puts "#{season.capitalize}:"
+      value.collect do |holiday, items|
+        puts "#{holiday.to_s.gsub("_", " ").split.map(&:capitalize).join(' ')}: #{items.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
